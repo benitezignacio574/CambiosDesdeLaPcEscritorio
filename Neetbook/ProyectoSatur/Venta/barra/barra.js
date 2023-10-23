3cd881f71bb348bcb4468b1a511dc737d1056678
IgnacioThomasBenitez <benitezignacio574@gmail.com>
2023-10-23 01:13:25 -0300

    Nuevo y Casi Ultimo Front

--- Neetbook/ProyectoSatur/Venta/barra/barra.js
+++ Neetbook/ProyectoSatur/Venta/barra/barra.js
@@ -1,5 +1,5 @@
 const search = document.querySelector('.input-group input'),
-    table_rows = document.querySelectorAll('body ul')
+    table_rows = document.querySelectorAll('body nav')
 
 search.addEventListener('input', searchTable);
 
@@ -12,7 +12,7 @@ function searchTable() {
         row.style.setProperty('--delay', i / 25 + 's');
     })
 
-    document.querySelectorAll('body ul:not(.hide)').forEach((visible_row, i) => {
+    document.querySelectorAll('body nav:not(.hide)').forEach((visible_row, i) => {
         visible_row.style.backgroundColor = (i % 2 == 0) ? 'transparent' : '#0000000b';
     });
 }
